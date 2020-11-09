c5e62f9488519ace02ce73f7434af52c5747d54e
Jan Reinhardt <Jan.Reinhardt@aitgmbh.de>
2020-11-09 22:41:25 +0100

    Sound gen

--- DisplayTest/main.cpp
+++ DisplayTest/main.cpp
@@ -7,7 +7,10 @@
 
 #define F_CPU 8000000UL
 
+#define PIN_OC0                    PORTB3
+
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 #include "DisplayHelper.h"
 #include "ModuleInterface.h"
@@ -15,6 +18,51 @@
 #include "BatteryModule.h"
 #include "TetrisModule.h"
 #include "BoxHelper.h"
+#include "waves.h"
+
+void initSound()
+{
+	// non inverting fast pwm
+	// prescaling 1
+	TCCR0 = (1<<WGM00)|(1<<WGM01)|(1<<COM01)|(1<<CS00);
+	
+	// Use this value to adjust pwm
+	OCR0 = 128;
+	
+	TIMSK |= (1<<TOIE0);
+	
+	// Set timer/pwm pin as output (OC0)
+	DDRB |= (1<<PIN_OC0);
+}
+
+uint8_t soundCount = 0;
+uint16_t hullCurveCounter = 0;
+uint8_t playCounter = 0;
+
+ISR(TIMER0_OVF_vect)
+{
+	// Somehow adjust OCR0 here...
+	soundCount++;
+	soundCount = soundCount % sizeof(sineWave_A4_440);
+	
+	hullCurveCounter++;
+	uint8_t h = hullCurve[hullCurveCounter>>8];	
+	
+	if(hullCurveCounter == 0){
+		playCounter++;
+	}
+	
+	if(playCounter&0x01)
+	{
+		uint16_t value = sineWave_A4_440[soundCount]*h;
+		
+		OCR0 = value>>8;
+	}
+	else
+	{
+		OCR0 = 0;	
+	}	
+}
 
 void init() 
 {
@@ -29,10 +77,13 @@ void init()
 	// Joystick & Switches (Pull Ups)
 	DDRA = 0x00;
 	PORTA = 0b11111110;
-	DDRB = 0x00;
+	
+	DDRB = 0b00000000;
 	PORTB = 0b00000111;
 
 	SPI_MasterInit();
+	
+	initSound();
 }
 
 void reset() 
@@ -65,6 +116,8 @@ void reset()
 int main(void)
 {
 	init();
+	
+	sei();
 	
 	reset();
 	
@@ -104,4 +157,3 @@ int main(void)
 		module->update();
 	}
 }
-
---
+++ WaveGen/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WaveGen
+{
+    class Program
+    {
+        static int f_base = 8000000;
+        static int f_pwm = f_base / 256;
+
+        static int mod(int x, int m) {
+            return (x%m + m)%m;
+        }
+
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine($"Base frequency: {f_base}");
+            Console.WriteLine($"PWM frequency: {f_pwm}");
+
+            string[] toneNames = new string[] {"C", "Cs", "D", "Ds", "E", "F", "Fs", "G", "Gs", "A", "As", "H"};
+            int octave = 4;
+            double baseTone = 440;
+
+            int minTone = -9;
+            int maxTone = 14;
+
+            var lines = new List<string>();
+
+            for(int i = minTone; i < maxTone; i++)
+            {
+                var index = i + 9;
+
+                var f = Math.Pow(2, (double)i/12) * baseTone;
+                var octaveNumber = octave + (index / toneNames.Length);
+
+                var toneName = toneNames[mod(index,toneNames.Length)] + octaveNumber;
+
+                double sampleCount = (double)f_pwm / f;
+                int sampleCountInt = (int)Math.Round(sampleCount);
+                var synthesisedFrequency = (double)f_pwm / sampleCountInt;
+                var err = Math.Abs(f - synthesisedFrequency)/f * 100;
+
+                Console.WriteLine($"{toneName.PadRight(8)}| {f:F2} | {synthesisedFrequency:F2} | {err:F2}% | {sampleCountInt.ToString().PadLeft(5)}");
+
+                lines.Add($"uint8_t sineWave_{toneName}_{f:F0}[] = ");
+                lines.Add("{");
+
+                for(int sample = 0; sample < sampleCountInt; sample++)
+                {
+                    var amplitude = Math.Sin(2*Math.PI*sample/sampleCountInt);
+
+                    var amplitudeByte = Math.Floor((amplitude+1.0)*0.5*256);
+                    amplitudeByte = Math.Min(255, amplitudeByte);
+                    amplitudeByte = Math.Max(0, amplitudeByte);
+
+                    lines.Add($"    {amplitudeByte},");
+                }
+
+                lines.Add("};");
+                lines.Add("");
+
+                
+            }
+
+            // Hull Curve
+            lines.Add($"uint8_t hullCurve[] = ");
+            lines.Add("{");
+
+            for(int sample = 0; sample < 256; sample++)
+            {
+                var amplitude = Math.Sin(Math.PI*sample/256);
+
+                var amplitudeByte = Math.Floor(amplitude*256);
+                amplitudeByte = Math.Min(255, amplitudeByte);
+                amplitudeByte = Math.Max(0, amplitudeByte);
+
+                lines.Add($"    {amplitudeByte},");
+            }
+
+            lines.Add("};");
+            lines.Add("");
+
+
+            var filename = "waves.h";
+
+            System.IO.File.WriteAllLines(filename, lines);
+
+            var filename2 = "../DisplayTest/waves.h";
+
+            System.IO.File.WriteAllLines(filename2, lines);
+        }
+    }
+}
